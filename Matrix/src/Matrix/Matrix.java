8acc6cde7a92c4fed4eebc7dbcc6e7bad83300be
Wenquan Zou <wenquan.zou16@imperial.ac.uk>
2017-11-26 18:53:02 +0000

    Add plus method to instance method and pass test

--- Matrix/src/Matrix/Matrix.java
+++ Matrix/src/Matrix/Matrix.java
@@ -6,20 +6,14 @@ public class Matrix {
 
   private double[][] elems;
 
+  //Create normal matrix.
   public Matrix(double[][] elems) {
     this.elems = elems;
   }
 
+  //Create matrix with dimension.
   public Matrix(int dimension) {
     this.elems = new double[dimension][dimension];
-  }
-
-  public Matrix identity(int dimension) {
-    double[][] result = new double[dimension][dimension];
-    for (int i = 0; i < dimension; i++) {
-      result[i][i] = 1;
-    }
-    return new Matrix(result);
   }
 
   @Override
@@ -37,7 +31,51 @@ public class Matrix {
     return Arrays.deepHashCode(elems);
   }
 
+  //Create identity matrix
+  public Matrix identity(int dimension) {
+    double[][] result = new double[dimension][dimension];
+    for (int i = 0; i < dimension; i++) {
+      result[i][i] = 1;
+    }
+    return new Matrix(result);
+  }
+
   public double[][] getElems() {
     return elems;
+  }
+
+  public void changeValue(int x, int y, double v) {
+    this.elems[x][y] = v;
+  }
+
+  private int getHoriDimen() {
+    return this.elems.length;
+  }
+
+  private int getVertDimen() {
+    return this.elems[0].length;
+  }
+
+  public double getElem(int x, int y) {
+    return this.elems[x][y];
+  }
+
+  //Plus matrix to the existing matrix.
+  public void plus(Matrix n) {
+
+    int h = this.elems.length;
+    int v = this.elems[0].length;
+
+    if (n.getHoriDimen() != h || n.getVertDimen() != v) {
+      throw new IllegalArgumentException("Matrix dimension mismatched");
+    }
+
+    //Plus operation on each matrix.
+    for (int i = 0; i < h; i++) {
+      for (int j = 0; j < v; j++) {
+        this.elems[i][j] += n.getElem(i, j);
+      }
+    }
+
   }
 }
--- Matrix/src/UnitTest/MatrixTest.java
+++ Matrix/src/UnitTest/MatrixTest.java
@@ -6,11 +6,25 @@ import org.junit.Test;
 import static junit.framework.TestCase.assertEquals;
 
 public class MatrixTest {
-
+  Matrix matrix = new Matrix(4);
   @Test
   public void createMatrixTest() {
-    Matrix matrix = new Matrix(4);
 
     assertEquals(matrix.getElems()[0][0], 0d);
+  }
+
+  @Test
+  public void enableToChangeElem() {
+    matrix.changeValue(2,3,8d);
+
+    assertEquals(matrix.getElem(2, 3), 8d);
+  }
+
+  @Test
+  public void plusTwoMatrix() {
+    double[][] m = {{0, 0, 0, 0}, {1, 1, 1, 1}, {2, 2, 2, 2}, {3, 3, 3, 3}};
+    matrix.plus(new Matrix(m));
+
+    assertEquals(matrix, new Matrix(m));
   }
 }
